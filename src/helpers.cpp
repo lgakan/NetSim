b84d8515d9aa2622f499bc8355871cd4299737f4
Łukasz Gakan <81683616+lgakan@users.noreply.github.com>
2022-06-21 02:51:03 +0200

    Comments repair

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -3,14 +3,13 @@
 #include <cstdlib>
 #include <random>
 
-// Do generowania wysokiej jakości ciągów liczb pseudolosowych warto użyć
-// zaawansowanych generatorów, np. algorytmu Mersenne Twister.
-// zob. https://en.cppreference.com/w/cpp/numeric/random
+// Generating high quality pseudorandom numbers with for example Mersenne Twister algorithm
+// look. https://en.cppreference.com/w/cpp/numeric/random
 std::random_device rd;
 std::mt19937 rng(rd());
 
 double default_probability_generator() {
-    // Generuj liczby pseudolosowe z przedziału [0, 1); 10 bitów losowości.
+    //Generate pseudo-random numbers from the interval [0, 1); 10 randomness bits.
     return std::generate_canonical<double, 10>(rng);
 }
 
